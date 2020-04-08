fbabbc52880dce62a18ca5814e0d0c0164d42536
Priyam Garg <gargpriyam21@gmail.com>
2020-04-09 02:28:55 +0530

    first commit

---
+++ Swift/MyPlayground.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+var str = "Hello, playground"
+
+//print("Hello World")
+
+var abc:String
+abc = "Priyam"
+print(abc)
+
+var abcd = "Priyam"
+abcd = "Aditya"
+print(abcd)
+
+for index in 1...5 {
+    print("\(index) times 5 is \(index * 5)")
+}
+
+let names = ["Anna", "Alex", "Brian", "Jack"]
+let count = names.count
+for i in 0..<count {
+    print("Person \(i + 1) is called \(names[i])")
+}
